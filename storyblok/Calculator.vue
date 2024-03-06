e639b5ccd0061226e17f44a852c27b7193e1d403
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2024-03-06 15:14:11 +0500

    responsive set

--- storyblok/Calculator.vue
+++ storyblok/Calculator.vue
@@ -22,7 +22,8 @@ const submitForm = () => {
 </script>
 <template>
   <div v-editable="blok" class="border-b-8 border-tertiary">
-    <section class="max-w-[1000px] mx-auto py-14 px-4">
+    <section class="max-w-[1000px] mx-auto py-8 px-4">
+      <!-- tabs is here -->
       <div
         class="block md:flex mx-auto max-w-[246px] md:max-w-[100%] justify-between border-b"
       >
@@ -53,122 +54,121 @@ const submitForm = () => {
               />
             </div>
           </div>
+          <!-- border bottom -->
           <div
             v-if="activeTab === index"
             class="w-[140px] h-[3px] bg-primary mt-3 ml-7 mb-[-8px]"
           ></div>
         </div>
       </div>
+      <!-- form is here -->
       <div
         v-show="activeTab !== null"
-        class="max-w-[700px] mx-auto mt-16 mb-8 p-12 pb-14 shadow-2xl"
+        class="max-w-[700px] mx-auto mt-16 mb-12 p-12 pb-14 shadow-2xl"
       >
-        <slot :tab="blok.tabs[activeTab]">
-          <form @submit.prevent="submitForm">
-            <div class="block md:flex items-center gap-2">
-              <label
-                for="inputField"
-                class="font-jakarta w-full md:w-[14%] text-black text-lg font-medium"
-                >Length (l)</label
+        <form @submit.prevent="submitForm">
+          <div class="block md:flex items-center gap-2">
+            <label
+              for="inputField"
+              class="font-jakarta w-full md:w-[14%] text-black text-lg font-medium"
+              >Length (l)</label
+            >
+            <div
+              class="grid grid-cols-1 md:grid-cols-2 gap-3 md:w-[86%] mt-2 md:mt-0"
+            >
+              <input
+                type="number"
+                class="border-primary border px-3 py-2 w-full font-jakarta"
+                placeholder="222"
+                v-model="lengthInput"
+              />
+              <select
+                id="length"
+                class="border border-black px-3 py-2 w-full font-jakarta"
               >
-              <div
-                class="grid grid-cols-1 md:grid-cols-2 gap-3 md:w-[86%] mt-2 md:mt-0"
-              >
-                <input
-                  type="number"
-                  class="border-primary border px-3 py-2 w-full font-jakarta"
-                  placeholder="222"
-                  v-model="lengthInput"
-                />
-                <select
-                  id="length"
-                  class="border border-black px-3 py-2 w-full font-jakarta"
-                >
-                  <option selected value="">M</option>
-                  <option value="US">L</option>
-                </select>
-              </div>
+                <option selected value="">M</option>
+                <option value="US">L</option>
+              </select>
             </div>
-            <div class="block md:flex items-center gap-2 mt-4">
-              <label
-                for="inputField"
-                class="font-jakarta w-full md:w-[14%] text-black text-lg font-medium"
-                >Width (w)</label
+          </div>
+          <div class="block md:flex items-center gap-2 mt-4">
+            <label
+              for="inputField"
+              class="font-jakarta w-full md:w-[14%] text-black text-lg font-medium"
+              >Width (w)</label
+            >
+            <div
+              class="grid grid-cols-1 md:grid-cols-2 gap-3 md:w-[86%] mt-2 md:mt-0"
+            >
+              <input
+                v-model="widthInput"
+                type="number"
+                class="border border-primary px-3 py-2 w-full font-jakarta"
+                placeholder="222"
+              />
+              <select
+                id="length"
+                class="border border-black px-3 py-2 w-full font-jakarta"
               >
-              <div
-                class="grid grid-cols-1 md:grid-cols-2 gap-3 md:w-[86%] mt-2 md:mt-0"
-              >
-                <input
-                  v-model="widthInput"
-                  type="number"
-                  class="border border-primary px-3 py-2 w-full font-jakarta"
-                  placeholder="222"
-                />
-                <select
-                  id="length"
-                  class="border border-black px-3 py-2 w-full font-jakarta"
-                >
-                  <option selected value="">M</option>
-                  <option value="US">L</option>
-                </select>
-              </div>
+                <option selected value="">M</option>
+                <option value="US">L</option>
+              </select>
             </div>
-            <div class="block md:flex items-center gap-2 mt-4">
-              <label
-                for="inputField"
-                class="font-jakarta w-full md:w-[14%] text-black text-lg font-medium"
-                >Depth (d)</label
+          </div>
+          <div class="block md:flex items-center gap-2 mt-4">
+            <label
+              for="inputField"
+              class="font-jakarta w-full md:w-[14%] text-black text-lg font-medium"
+              >Depth (d)</label
+            >
+            <div
+              class="grid grid-cols-1 md:grid-cols-2 gap-3 md:w-[86%] mt-2 md:mt-0"
+            >
+              <input
+                v-model="depthInput"
+                type="number"
+                class="border border-primary px-3 py-2 w-full font-jakarta"
+                placeholder="222"
+              />
+              <select
+                id="length"
+                class="border border-black px-3 py-2 w-full font-jakarta"
               >
-              <div
-                class="grid grid-cols-1 md:grid-cols-2 gap-3 md:w-[86%] mt-2 md:mt-0"
-              >
-                <input
-                  v-model="depthInput"
-                  type="number"
-                  class="border border-primary px-3 py-2 w-full font-jakarta"
-                  placeholder="222"
-                />
-                <select
-                  id="length"
-                  class="border border-black px-3 py-2 w-full font-jakarta"
-                >
-                  <option selected value="">M</option>
-                  <option value="US">L</option>
-                </select>
-              </div>
+                <option selected value="">M</option>
+                <option value="US">L</option>
+              </select>
             </div>
-            <div class="block md:flex items-center gap-2 mt-4 md:mt-14">
-              <label
-                for="inputField"
-                class="font-jakarta w-full md:w-[14%] text-black text-lg font-medium"
-                >Quantity</label
-              >
-              <div class="md:w-[85%] mt-2 md:mt-0">
-                <input
-                  v-model="quantityInput"
-                  type="number"
-                  class="border border-primary px-3 py-2 w-full font-jakarta"
-                  placeholder="222"
-                />
-              </div>
+          </div>
+          <div class="block md:flex items-center gap-2 mt-4 md:mt-14">
+            <label
+              for="inputField"
+              class="font-jakarta w-full md:w-[14%] text-black text-lg font-medium"
+              >Quantity</label
+            >
+            <div class="md:w-[85%] mt-2 md:mt-0">
+              <input
+                v-model="quantityInput"
+                type="number"
+                class="border border-primary px-3 py-2 w-full font-jakarta"
+                placeholder="222"
+              />
             </div>
-
-            <div class="flex justify-center gap-3">
-              <button
-                type="submit"
-                class="max-w-[130px] px-5 py-2 text-lg font-jakarta mt-8 block cursor-pointer bg-primary text-white"
-              >
-                Calculate
-              </button>
-              <button
-                type="reset"
-                class="max-w-[100px] px-5 py-2 text-lg font-jakarta mt-8 block cursor-pointer bg-white text-primary border-primary border"
-              >
-                Reset
-              </button>
-            </div>
-          </form>
-        </slot>
+          </div>
+          <div class="flex justify-center gap-3">
+            <button
+              type="submit"
+              class="max-w-[130px] px-5 py-2 text-lg font-jakarta mt-8 block cursor-pointer bg-primary text-white"
+            >
+              Calculate
+            </button>
+            <button
+              type="reset"
+              class="max-w-[100px] px-5 py-2 text-lg font-jakarta mt-8 block cursor-pointer bg-white text-primary border-primary border"
+            >
+              Reset
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   </div>
